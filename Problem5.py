a3154fc59267dd4e6f44b9f2455ae3f9efbf6082
Roman Petrica <romanpetrica2895@gmail.com>
2019-10-13 16:50:29 +0300

    problem 5

---
+++ Problem5.py
@@ -0,0 +1,15 @@
+"""
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+"""
+
+""" BRUTE FORCE
+number = 20
+while True:
+    if number % 11 == 0 and number % 12 == 0 and number % 13 == 0 and number % 14 == 0 and number % 15 == 0 and number % 16 == 0 and number % 17 == 0 and number % 18 == 0 and number % 19 == 0 and number % 20 == 0:
+        break;
+    number = number + 20
+print(number)
+"""
